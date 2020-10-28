393c8449fecbc1b50b9c7afbb859cef6fc95edbe
Lili Cohen <lili.cohen.tv@gmail.com>
2020-10-28 14:57:32 -0700

    adding files

---
+++ Assets/javascript/script.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+var myQuestions = [
+	{
+		question: "What does HTML stand for?",
+		answers: {
+			a: 'Hyper Text Markup Language',
+			b: 'Hyperlinks and Text Markup Language',
+			c: 'Hyper Tool Markup Language'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: "Which one of these is an example of a boolean?",
+		answers: {
+			a: 'true',
+			b: '12',
+			c: 'Big Bird'
+		},
+		correctAnswer: 'a'
+    },
+    {
+		question: "In the following array, what would the index of dog be? Ex: myAnimals = [cat, dog, parrot]",
+		answers: {
+			a: '0',
+			b: '1',
+			c: '2'
+		},
+		correctAnswer: 'b'
+    },
+    {
+		question: "",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+    },
+    {
+		question: "What is 30/3?",
+		answers: {
+			a: '3',
+			b: '5',
+			c: '10'
+		},
+		correctAnswer: 'c'
+	}
+];
