9d81e844fc3c8eb4b4c59677cdcf5f3935ac591a
Lili Cohen <lili.cohen.tv@gmail.com>
2020-10-30 12:06:40 -0700

    style creating better hide class, javascript adding new functions endgame displaying answers to page, trying to set up local storage

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -2,7 +2,8 @@ var qContainer = document.getElementById("quiz-container");
 var quizIntro = document.getElementById("quiz-intro");
 var startBtn = document.getElementById("start");
 var quizContent = document.getElementById("quiz");
-var quizQuestions = document.getElementsByClassName("questions");
+var questionPrompt = document.getElementById("questionPrompt");
+var quizQuestions = document.getElementById("questions");
 var choiceA = document.getElementById("A");
 var choiceB = document.getElementById("B");
 var choiceC = document.getElementById("C");
@@ -10,8 +11,7 @@ var timeEl = document.getElementById("timer");
 var scoreDiv = document.getElementById("score");
 
 //My questions for the quiz
-var myQuestions = [
-	{
+var myQuestions = [{
 		question: "What does HTML stand for?",
 		answers: {
 			a: 'Hyper Text Markup Language',
@@ -28,8 +28,8 @@ var myQuestions = [
 			c: 'Big Bird'
 		},
 		correctAnswer: 'a'
-    },
-    {
+	},
+	{
 		question: "In the following array, what would the index of dog be? Ex: myAnimals = [cat, dog, parrot]",
 		answers: {
 			a: '0',
@@ -37,8 +37,8 @@ var myQuestions = [
 			c: '2'
 		},
 		correctAnswer: 'b'
-    },
-    {
+	},
+	{
 		question: "Which one of these is an example of a class in CSS?",
 		answers: {
 			a: '#dragon',
@@ -46,8 +46,8 @@ var myQuestions = [
 			c: '.monster-mash'
 		},
 		correctAnswer: 'c'
-    },
-    {
+	},
+	{
 		question: "How does a computer read code?",
 		answers: {
 			a: 'Top down',
@@ -61,29 +61,92 @@ var myQuestions = [
 //Variables for time and score
 var secondsLeft = 30;
 var correct = 0;
+var currentQuestionIndex = 0
+var scores = getScores()
 
 // Timer counts down 30 seconds
 function setTime() {
-    var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft;
-      
-        if(secondsLeft === 0) {
-            clearInterval(timerInterval);
-            
-        }
-        console.log(timerInterval);
-    }, 1000);
+	var timerInterval = setInterval(function () {
+		secondsLeft--;
+		timeEl.textContent = secondsLeft;
+
+		if (secondsLeft <= 0) {
+			clearInterval(timerInterval);
+			endGame()
+		}
+		console.log(timerInterval);
+	}, 1000);
+}
+
+function checkAnswer(clickedAnswer) {
+	var lower = clickedAnswer.toLowerCase()
+
+	if (lower === myQuestions[currentQuestionIndex].correctAnswer) {
+		// If the answer is correct
+		correct++
+	} else {
+		// Otherwise
+		secondsLeft -= 5
+	}
+
+	// TODO - Move to next question
+	currentQuestionIndex++
+
+	if (currentQuestionIndex >= myQuestions.length) {
+		// TODO - End the game
+		endGame()
+		return
+	}
+
+	showQuestion()
+}
+
+function endGame() {
+	console.log("END")
+	// TODO - Show end game screen
+}
+
+function restartGame() {
+	secondsLeft = 30
+	currentQuestionIndex = 0
+	correct = 0
+
+	// TODO - Hide the end game screen 
+	startQuiz()
+}
+
+function showQuestion() {
+	questionPrompt.textContent = myQuestions[currentQuestionIndex].question;
+	choiceA.textContent = myQuestions[currentQuestionIndex].answers.a;
+	choiceB.textContent = myQuestions[currentQuestionIndex].answers.b;
+	choiceC.textContent = myQuestions[currentQuestionIndex].answers.c;
 }
 
 // trying to create a for loop for my questions
 function startQuiz() {
-    setTime();
-	quizQuestions.textContent = myQuestions.question;
-	console.log(myQuestions.question);
+	setTime();
+	showQuestion()
+
 }
 
-startBtn.addEventListener("click", function() {
-    startQuiz();
-   
+startBtn.addEventListener("click", function () {
+	startQuiz();
 });
+
+function showSubmitButton() {
+	document.getElementById("submit").style.display = "block"
+}
+
+function saveScore(initials, score) {
+	scores.push({
+		initials,
+		score
+	})
+	localStorage.setItem('scores', JSON.stringify(scores))
+}
+
+function getScores() {
+	return JSON.parse(
+		localStorage.getItem('scores')
+	) || []
+}
